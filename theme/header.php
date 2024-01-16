00989c1bafcdf9af4df557f99f6865712b076a30
shubhangi831 <shubhangi099kamble@gmail.com>
2024-01-16 18:27:29 +0530

    Modify header and header-content and nav-menu.css file

--- theme/header.php
+++ theme/header.php
@@ -24,7 +24,7 @@
 <?php wp_body_open(); ?>
 
 <div id="page">
-	<a href="#content" class="sr-only"><?php esc_html_e( 'Skip to content', 'upgo-tech-tw' ); ?></a>
+	<!-- <a href="#content" class="sr-only"><?php //esc_html_e( 'Skip to content', 'upgo-tech-tw' ); ?></a> -->
 
 	<?php get_template_part( 'template-parts/layout/header', 'content' ); ?>
 
